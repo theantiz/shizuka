c05458726c8a4d1f57b1fed38a84a2efe4288889
theantiz <jaychothiyawala04@gmail.com>
2025-12-13 05:25:05 +0530

    QUIC Netty Error Fix v1

--- backendAPIs/src/main/java/xyz/antiz/shizuka/WebClientConfig.java
+++ backendAPIs/src/main/java/xyz/antiz/shizuka/WebClientConfig.java
@@ -1,12 +1,17 @@
 package xyz.antiz.shizuka;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
 @Configuration
 public class WebClientConfig {
 
     @Bean
     public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
+        HttpClient httpClient = HttpClient.create();
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient));
     }
 }
